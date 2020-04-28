c1b8ac92a3179065915aa3773f6aba773888ccb5
Akaash Kannan <akaash324it@gmail.com>
2020-04-28 19:40:11 +0530

    optimization done

--- src/App.js
+++ src/App.js
@@ -4,55 +4,56 @@ import Pagination from "./components/Pagination";
 import axios from "axios";
 import "./App.css";
 
+const selectOptions = [5, 10, 20, 30];
+
 const App = () => {
   const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(5);
-  const selectOptions = [5, 10, 20, 30];
+
+  const totalLengthOfPosts = posts.length;
+  const indexOfLastPost = currentPage * postsPerPage;
+
+  //fetch data for api endpoint
+  const fetchPosts = async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    setPosts(res.data);
+  };
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-      setPosts(res.data);
-    };
-
     fetchPosts();
   }, []);
 
   // Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const currentPosts = () => {
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    return posts.slice(indexOfFirstPost, indexOfLastPost);
+  };
 
   // Change page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  const handlePostsPerPageChange = (event) => {
-    setPostsPerPage(event.target.value);
+  const enableButton = () => {
+    return (
+      indexOfLastPost > postsPerPage * (currentPage - 1) &&
+      indexOfLastPost < totalLengthOfPosts
+    );
   };
-
-  const handleChangePrevPage = () => {
-    setCurrentPage(currentPage - 1);
-  };
-
-  const handleChangeNextPage = () => {
-    setCurrentPage(currentPage + 1);
-  };
-
-  const calculateValue = postsPerPage * currentPage;
-  const totalLengthOfPosts = posts.length;
-  const enableButton =
-    calculateValue > postsPerPage * (currentPage - 1) &&
-    calculateValue < totalLengthOfPosts;
 
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">React Posts</h1>
-      <Posts posts={currentPosts} />
-      <button disabled={currentPage === 1} onClick={handleChangePrevPage}>
+      <Posts posts={currentPosts()} />
+      <button
+        disabled={currentPage === 1}
+        onClick={() => setCurrentPage(currentPage - 1)}
+      >
         Prev
       </button>
-      <button disabled={!enableButton} onClick={handleChangeNextPage}>
+      <button
+        disabled={!enableButton()}
+        onClick={() => setCurrentPage(currentPage + 1)}
+      >
         Next
       </button>
       <Pagination
@@ -61,7 +62,7 @@ const App = () => {
         paginate={paginate}
       />
       <br />
-      <select onChange={handlePostsPerPageChange}>
+      <select onChange={(event) => setPostsPerPage(event.target.value)}>
         {selectOptions.map((value) => (
           <option key={value}>{value}</option>
         ))}
